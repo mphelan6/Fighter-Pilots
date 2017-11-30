f0c3e7bd495a0f03f0b9e311525e071b4255ab05
Charles <charlesbwalberg@gmail.com>
2017-11-29 20:53:18 -0500

    rando

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : Photon.PunBehaviour {
+    public PhotonLogLevel Loglevel = PhotonLogLevel.Informational;
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    public byte MaxPlayersPerRoom = 4;
+
+    string _gameVersion = "1";
+
+    void Awake()
+    {
+        // #Critical
+        // we don't join the lobby. There is no need to join a lobby to get the list of rooms.
+        PhotonNetwork.autoJoinLobby = false;
+
+        // #Critical
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+        PhotonNetwork.automaticallySyncScene = true;
+
+        PhotonNetwork.logLevel = Loglevel;
+    }
+
+
+    // MonoBehaviour method called on GameObject by Unity during initialization phase.
+    void Start()
+    {
+
+    }
+
+    // Start the connection process. 
+    // If already connected, we attempt joining a random room
+    // - if not yet connected, Connect this application instance to Photon Cloud Network
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.connected) {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnPhotonRandomJoinFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        } else {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        }
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+        // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()  
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+
+    public override void OnDisconnectedFromPhoton() {
+        Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg) {
+        Debug.Log("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = MaxPlayersPerRoom }, null);
+    }
+
+    public override void OnJoinedRoom() {
+        Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+}
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Player name input field. Let the user input his name, will appear above the player in the game.
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField : MonoBehaviour
+{
+    // Store the PlayerPref Key to avoid typos
+    static string playerNamePrefKey = "PlayerName";
+
+    // MonoBehaviour method called on GameObject by Unity during initialization phase.
+    void Start() {
+        string defaultName = "";
+        InputField _inputField = this.GetComponent<InputField>();
+        if (_inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                _inputField.text = defaultName;
+            }
+        }
+        PhotonNetwork.playerName = defaultName;
+    }
+
+    // Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+    // <param name="value">The name of the Player</param>
+    public void SetPlayerName(string value) {
+        // #Important
+        PhotonNetwork.playerName = value + " "; // force a trailing space string in case value is an empty string, else playerName would not be updated.
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
