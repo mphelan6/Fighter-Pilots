a0b10a87b300af23d6c5a8771e96be97cd25508c
Michael Phelan <mphelan6@villanova.edu>
2017-12-06 13:13:49 -0500

    added a prefab for the spare part

---
+++ Assets/Scripts/part_rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class part_rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(30*Time.deltaTime, 40*Time.deltaTime, 90*Time.deltaTime));
+	}
+}
